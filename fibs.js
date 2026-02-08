6dd9f2767af632988f647aa93b6db080e986e6bb
ophanim777 <rikyjo49@gmail.com>
2026-02-08 22:13:09 +0700

    add recursion function

--- fibs.js
+++ fibs.js
@@ -13,3 +13,14 @@ function fibs(n) {
 }
 
 console.log(fibs(8)); 
+
+function fibsRec(n) {
+  console.log("This was printed recursively");
+
+  if (n <= 0) return [];
+  if (n === 1) return [0];
+  if (n === 2) return [0, 1];
+
+  const prev = fibsRec(n - 1);
+  return [...prev, prev[prev.length - 1] + prev[prev.length - 2]];
+}
