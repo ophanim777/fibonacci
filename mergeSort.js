ef3a58bb887b765922b4e022f354adeb0133b9d5
ophanim777 <rikyjo49@gmail.com>
2026-02-08 22:20:42 +0700

    add mergeSort function

---
+++ mergeSort.js
@@ -0,0 +1,23 @@
+function mergeSort(array) {
+  if (array.length <= 1) return array;
+
+  const mid = Math.floor(array.length / 2);
+  const left = mergeSort(array.slice(0, mid));
+  const right = mergeSort(array.slice(mid));
+
+  return merge(left, right);
+}
+
+function merge(left, right) {
+  const sorted = [];
+
+  while (left.length && right.length) {
+    if (left[0] <= right[0]) {
+      sorted.push(left.shift());
+    } else {
+      sorted.push(right.shift());
+    }
+  }
+
+  return [...sorted, ...left, ...right];
+}
