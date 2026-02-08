0c68c8573f1d5d520772cc1ea0628e034de01be0
ophanim777 <rikyjo49@gmail.com>
2026-02-08 22:09:41 +0700

    add iteration function for fibonacci sequence

---
+++ fibs.js
@@ -0,0 +1,12 @@
+function fibs(n) {
+  const result = [];
+
+  if (n <= 0) return result;
+  if (n >= 1) result.push(0);
+  if (n >= 2) result.push(1);
+
+  for (let i = 2; i < n; i++) {
+    result.push(result[i - 1] + result[i - 2]);
+  }
+
+}
