508bf21d16557286149f89eec5b8eca6ce111e65
ophanim777 <rikyjo49@gmail.com>
2026-02-08 22:21:41 +0700

    add console log

--- mergeSort.js
+++ mergeSort.js
@@ -21,3 +21,13 @@ function merge(left, right) {
 
   return [...sorted, ...left, ...right];
 }
+
+console.log(mergeSort([])); 
+
+console.log(mergeSort([73])); 
+
+console.log(mergeSort([1, 2, 3, 4, 5])); 
+
+console.log(mergeSort([3, 2, 1, 13, 8, 5, 0, 1])); 
+
+console.log(mergeSort([105, 79, 100, 110])); 
